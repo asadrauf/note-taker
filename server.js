d12ec71d096cd53211b05608543e876cdf9684ec
ARauf Git <umiasad2017@gmail.com>
2020-03-15 08:01:12 -0400

    added express server and set up initial port for the listener. Also added apiRoutes and htmlRoutes

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening port: ${PORT}`);
+});
